e5bb588e46a7a83305992fb7431f4b306e7837c7
badalkr2004 <badalkr279@gmail.com>
2025-06-15 22:37:01 +0530

    change: backend port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
     message: 'Welcome to the luxe roofing api',
   });
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
