9f95014c5452b0a5364532832a5339923cdc6852
fendrick <its.fendrick@gmail.com>
2025-07-05 00:19:42 +0530

    fix:card

--- frontend/src/components/admin/login-form.tsx
+++ frontend/src/components/admin/login-form.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui";
+import { CardContent } from "@/components/ui/Card"
 import { Input } from "@/components/ui";
 import { Label } from "@/components/ui/label";
 import { useForm } from "react-hook-form";
--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -6,7 +6,7 @@ export { Select } from './select';
 export { Textarea } from './textarea';
 export { Switch } from './switch';
 // export { Radio } from './r
-export { Card } from './card';
+export { Card } from './Card';
 export { Tooltip } from './tooltip';
 export {Alert} from './Alert';
 export { LoadingSpinner } from './LoadingSpinner';
