1d5375bc05dffb8e5a2151491e089669819ee3b0
badalkr2004 <badalkr279@gmail.com>
2025-07-18 02:20:42 +0530

    fix: some linting issue fix

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -1,11 +1,10 @@
-export { Button } from "./Button"
+export { Button } from "./button";
 export { Input } from "./Input";
 // export { Modal } from './Modal';
 export { Checkbox } from "./checkbox";
 export { Select } from "./select";
 export { Textarea } from "./textarea";
 export { Switch } from "./switch";
-// export { Radio } from './r
 
 export { Card } from "./Card";
 export { Tooltip } from "./tooltip";
