bea06dfd68729d1295c25d7bec59f6c75a64e36b
fendrick <its.fendrick@gmail.com>
2025-07-25 01:09:17 +0530

    fix:deleteunusedfiles

--- frontend/src/types/custom-elements.d.ts
+++
@@ -1,9 +0,0 @@
-// custom-elements.d.ts
-declare namespace JSX {
-  interface IntrinsicElements {
-    'gmp-map': any;
-    'gmp-advanced-marker': any;
-    'gmp-place-picker': any;
-    // Add more if needed
-  }
-}
